d9370fa549859c657a1dc4ee09d72e742282ed98
Danny Nielsen <danny.nlsn0@gmail.com>
2021-04-30 23:53:17 +0200

    sql-queries/ folder cleanup, proper query renaming

--- sql-queries/fkd.sql
+++ sql-queries/fkd.sql
@@ -1,8 +1,7 @@
 SELECT
-	fks.table_schema,
+  fks.TABLE_SCHEMA,
 	fks.table_name,
-	fks.constraint_name,
-	fks.constraint_type 
+	fks.constraint_name
 FROM
 	information_schema.table_constraints fks,
 	information_schema.TABLES tab 
@@ -11,7 +10,7 @@ WHERE
 	AND fks.table_name = tab.table_name 
 	AND fks.constraint_type = 'FOREIGN KEY' 
 	AND tab.table_type = 'BASE TABLE' 
-	AND tab.table_schema NOT IN ( 'mysql', 'information_schema', 'performance_schema', 'sys' ) 
+	AND tab.table_schema = DATABASE()
 ORDER BY
 	tab.table_schema,
 	tab.table_name;
---
+++ sql-queries/pkd.sql
@@ -0,0 +1,21 @@
+SELECT
+	tab.table_schema AS database_schema,
+	sta.index_name AS pk_name,
+	sta.seq_in_index AS column_id,
+	sta.column_name,
+	tab.table_name,
+	cls.data_type 
+FROM
+	information_schema.TABLES AS tab
+	INNER JOIN information_schema.statistics AS sta ON sta.table_schema = tab.table_schema 
+	AND sta.table_name = tab.table_name 
+	AND sta.index_name = 'primary'
+	JOIN information_schema.`COLUMNS` AS cls ON tab.TABLE_SCHEMA = cls.TABLE_SCHEMA 
+	AND tab.TABLE_NAME = cls.TABLE_NAME 
+	AND sta.column_name = cls.column_name 
+WHERE
+	tab.table_schema = DATABASE()
+	AND tab.table_type = 'BASE TABLE' 
+ORDER BY
+	tab.table_name,
+	column_id;
