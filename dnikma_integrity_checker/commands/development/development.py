65ac55cc2ebcbc4eb0d0474d459fcde237983bcc
Danny Nielsen <danny.nlsn0@gmail.com>
2021-05-03 11:48:25 +0200

    Minor pydoc changes

--- dnikma_integrity_checker/commands/development/development.py
+++ dnikma_integrity_checker/commands/development/development.py
@@ -4,11 +4,11 @@ from ..connect.connect import open_connection
 
 
 @command(':development')
-@argument('schema', choices=['northwind_nofks', 'northwind'])
+@argument('schema', choices=['northwind_nofks', 'northwind'], description="Choose development schema")
 def development(schema: str = 'northwind_nofks'):
     """
     Activate development mode.
-    A MySQL connection to a local development schema will be established ('northwind_nofks' is the default choice).
+    A MySQL connection to a local development schema will be established.
     --------------------------------------------------------------------------------------------------------------------
     """
     open_connection({'host': '127.0.0.1', 'database': schema, 'user': 'root'})
