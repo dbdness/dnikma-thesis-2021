d72ffe0ea9e388c09c23864fc14b13dee948886d
Danny Nielsen <danny.nlsn0@gmail.com>
2021-05-06 16:06:11 +0200

    pfkd-v1: Downgrade to proper version for VC

--- sql-queries/pfkd-v1.sql
+++ sql-queries/pfkd-v1.sql
@@ -1,5 +1,5 @@
 SET
-    @db = DATABASE(),
+    @db = 'northwind_nofks',
     @pk_nullable = 'NO'; -- for some DB designs, this might be 'YES'.
 
 SELECT
@@ -7,7 +7,7 @@ SELECT
         REPLACE (
             REPLACE (
                 REPLACE (
-                    'SELECT ''{Ltable}.{Lcol}'' as left_col, ''{Rtable}.{Rcol}'' as right_col, COUNT(l.{Lcol}) as count_left, COUNT(r.{Rcol}) as count_right, COUNT(r.{Rcol})-COUNT(l.{Lcol}) as diff, COUNT(DISTINCT(l.{Lcol})) as distinct_left, COUNT(DISTINCT(r.{Rcol})) as distinct_right,
+                    'SELECT ''{Ltable}.{Lcol}'' as left_col, ''{Rtable}.{Rcol}'' as right_col, COUNT(l.{Lcol}) as count_left, COUNT(r.{Rcol}) as count_right,
                     CASE WHEN count(r.{Rcol}) = 0 
                     THEN 0 
                     ELSE COUNT(l.{Lcol})/COUNT(r.{Rcol})
