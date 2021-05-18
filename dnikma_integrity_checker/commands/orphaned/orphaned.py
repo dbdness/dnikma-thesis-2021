6f1f5d2c7d63ff9c25e850eabf674c186f5a3907
Danny Nielsen <danny.nlsn0@gmail.com>
2021-05-18 12:31:48 +0200

    First version of both working orphaned versions

--- dnikma_integrity_checker/commands/orphaned/orphaned.py
+++ dnikma_integrity_checker/commands/orphaned/orphaned.py
@@ -1,8 +1,10 @@
+import re
+
 from nubia import command, argument, context
 
 from dnikma_integrity_checker.commands.db_connect.mysql.mysql_conn import MySQLConn
 from dnikma_integrity_checker.helpers.utils import dicprint, Severity, get_row_at_pos, read_sql_file, \
-    DicLoadingSpinner, dicprint_table
+    DicLoadingSpinner, dicprint_table, db_ok
 from dnikma_integrity_checker.shell.configs.dic_context import DicContext
 
 query = read_sql_file('orphaned.sql')
@@ -39,8 +41,10 @@ class Orphaned:
         Find orphaned records from parent/child record output from the 'pfkd' command.
         Note that the "pfkd" command must be run at least once before this command.
         """
+        if not db_ok(self.db):
+            return
+
         ids = [i.strip() for i in pk_ids.split(',')]
-        # if any(not isinstance(val, int) for val in ids):
         try:
             ids = [int(i) for i in ids]
         except ValueError:
@@ -52,70 +56,71 @@ class Orphaned:
             pfkd_out = [get_row_at_pos(pfkd_out, i) for i in ids]
             pairs = [p[-4:] for p in pfkd_out]  # Stripping all but last 4 helper-columns
 
-            def query_builder() -> []:
-                rows = []
-                for p in pairs:
-                    curs = self.db.query(query, params={'child_tab': p[2],
-                                                        'child_col': p[3],
-                                                        'parent_tab': p[0],
-                                                        'parent_col': p[1]})
-                    r = curs.fetchone()
-                    rows.append(r)
-                return rows
-
-            qb_rows = _run_orphaned(query_builder)
-
-            def orphaned() -> []:
-                rows = []
-                for q in qb_rows:
-                    curs = self.db.query(q[0])
-                    r = curs.fetchall()
-                    rows.extend(r)  # extend() is called, as r may be a list of n results.
-                return rows
-
-            orows = _run_orphaned(orphaned)
+            qb_rows = _run_orphaned(pairs, self._query_builder)
+            orows = _run_orphaned(qb_rows, self._orphaned)
         dicprint_table(orows, orphaned_cols)
 
     @command()
     @argument('parent',
-              description="Parent table and column. Format: '{parent_table}.{parent_column}'",
+              description='Parent table and column. Format: "{parent_table}.{parent_column}"',
               type=str)
     @argument('child',
-              description="Child table and column. Format: '{child_table}.{child_column}'",
+              description='Child table and column. Format: "{child_table}.{child_column}"',
               type=str)
     def input(self, parent: str, child: str):
         """
         Find orphaned records from manually specified parent/child record input.
         """
-        pass
+        if not db_ok(self.db):
+            return
 
-    # def _run_orphaned_query_builder(self, pairs: []) -> []:
-    #     rows = []
-    #     try:
-    #         for pair in pairs:
-    #             row = self.db.query(query, params={'child_tab': pair[0],
-    #                                                'child_col': pair[1],
-    #                                                'parent_tab': pair[2],
-    #                                                'parent_col': pair[3]})
-    #             rows.append(row)
-    #     except Exception as ex:
-    #         dicprint("An unknown error occurred. We are very sorry. Details:", Severity.ERROR)
-    #         dicprint(f'{ex}', Severity.NONE)
-    #         dicprint(
-    #             "Do not hesitate to reach out to us with an issue on the official GitHub repository, "
-    #             "describing the error in detail.",
-    #             Severity.INFO)
-    #     finally:
-    #         return rows
+        regexp = r'\{(.*?)\}'  # Fetching strings between starting- ({) and closing (}) brackets
+
+        mp = re.findall(regexp, parent)
+        mc = re.findall(regexp, child)
+
+        if (not mp or not mc) or (len(mp) < 2 or len(mc) < 2):
+            dicprint("Error: Could not parse input parent- or child tables and columns.", Severity.ERROR)
+            dicprint('Please make sure to follow the format of: "{parent_table}.{parent_column"', Severity.INFO)
+            return
+
+        pairs = [(mp + mc)]  # Transforming pairs to fit the _query_builder function
+
+        with DicLoadingSpinner():
+            qb_rows = _run_orphaned(pairs, self._query_builder)
+            if None in qb_rows:
+                dicprint("Error: Could not find specified table/column combinations in the current schema.",
+                         Severity.ERROR)
+                dicprint(f"Please verify input parent/child table/column combinations: {mp}, {mc}",
+                         Severity.INFO)
+                return
+            orows = _run_orphaned(qb_rows, self._orphaned)
+        dicprint_table(orows, orphaned_cols)
+
+    def _query_builder(self, pairs: []):
+        rows = []
+        for p in pairs:
+            curs = self.db.query(query, params={'child_tab': p[2],
+                                                'child_col': p[3],
+                                                'parent_tab': p[0],
+                                                'parent_col': p[1]})
+            r = curs.fetchone()
+            rows.append(r)
+        return rows
+
+    def _orphaned(self, qb_rows) -> []:
+        rows = []
+        for q in qb_rows:
+            curs = self.db.query(q[0])  # Index 0, as we know there is only one output from orphaned query builder.
+            r = curs.fetchall()
+            rows.extend(r)  # extend() is called, as r may be a list of n results.
+        return rows
 
 
-def _run_orphaned(inner_func) -> []:
+def _run_orphaned(elements: [], inner_func) -> []:
     rows = []
     try:
-        rows = inner_func()
-        # for q in qb_queries:
-        #     row = self.db.query(q)
-        #     rows.append(row)
+        rows = inner_func(elements)
     except Exception as ex:
         dicprint("An unknown error occurred. We are very sorry. Details:", Severity.ERROR)
         dicprint(f'{ex}', Severity.NONE)
