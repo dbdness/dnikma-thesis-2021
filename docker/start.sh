812714a8398481c39695198c13b92efdf0eb4862
Danny Nielsen <danny.nlsn0@gmail.com>
2021-05-29 20:45:39 +0200

    #22: Cleanup Dockerfile and setup.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-#/usr/bin/mysqld_safe --skip-grant-tables &
+echo "Entering container..."
+echo "Starting MySQL server..."
 /usr/bin/mysqld_safe &
 sleep 5
+echo "Importing sample databases..."
 mysql -u root -t < infrastructure/norhwind/northwind-nofks-setup.sql
 mysql -u root -e "CREATE USER 'demo'@'localhost' IDENTIFIED BY 'demo'"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'demo'@'localhost'"
-#/bin/bash
+echo "Done!"
+echo "Starting dIC..."
 dic
